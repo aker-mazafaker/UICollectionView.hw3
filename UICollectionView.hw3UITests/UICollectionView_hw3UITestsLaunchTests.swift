8343933a5597e14b8de9eabc4927ba3349b33dac
Aker <@akerke.n@inbox.ru>
2023-09-11 23:23:27 +0600

    Initial Commit

---
+++ UICollectionView.hw3UITests/UICollectionView_hw3UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  UICollectionView_hw3UITestsLaunchTests.swift
+//  UICollectionView.hw3UITests
+//
+//  Created by Akerke on 11.09.2023.
+//
+
+import XCTest
+
+final class UICollectionView_hw3UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
